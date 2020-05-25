7364198d9fcc567fcff459a3437e5b520d1f1225
shane01101 <shane.honanie@fluentworlds.com>
2020-05-24 19:20:43 -0700

    main window

---
+++ image-shrink/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: 500,
+    height: 600,
+    icon: './assets/icons/Icon_256x256.png',
+  })
+
+  //   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  mainWindow.loadFile('./app/index.html')
+}
+
+app.on('ready', createMainWindow)
