95171e3b4190e5e9cf3aa3020d934a283ffda15b
shane01101 <shane.honanie@fluentworlds.com>
2020-05-25 11:32:04 -0700

    create menu for mainWindow

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 process.env.NODE_ENV = 'development'
 
@@ -6,6 +6,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let aboutWindow
 
 const createMainWindow = () => {
   mainWindow = new BrowserWindow({
@@ -14,12 +15,78 @@ const createMainWindow = () => {
     height: 600,
     icon: './assets/icons/Icon_256x256.png',
     resizable: isDev ? true : false,
+    backgroundColor: 'white',
   })
 
   mainWindow.loadFile('./app/index.html')
 }
 
-app.on('ready', createMainWindow)
+const createAboutWindow = () => {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'white',
+  })
+
+  aboutWindow.loadFile('./app/about.html')
+  aboutWindow.setMenuBarVisibility(false)
+}
+
+app.on('ready', () => {
+  createMainWindow()
+
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+  mainWindow.on('ready', () => (mainWindow = null))
+})
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+  ...(!isMac
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+]
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
